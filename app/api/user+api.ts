6cca20ec9e8c9ec11acab2609c0a4b42b92b11bb
ramprit123 <ramprit.sahani@outlook.com>
2025-05-21 09:14:34 +0530

    feat: update app configuration and add user API endpoints

---
+++ app/api/user+api.ts
@@ -0,0 +1,15 @@
+export function GET(request: Request) {
+  return Response.json({ hello: 'world' });
+}
+export function POST(request: Request) {
+  return Response.json({ hello: 'world' });
+}
+export function PUT(request: Request) {
+  return Response.json({ hello: 'world' });
+}
+export function DELETE(request: Request) {
+  return Response.json({ hello: 'world' });
+}
+export function PATCH(request: Request) {
+  return Response.json({ hello: 'world' });
+}
